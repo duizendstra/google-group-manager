dc2f16338a55571a6215ee709dbee97e92b48e61
Jasper Duizendstra <jasper.duizendstra@g-workplace.com>
2016-10-18 09:27:20 +0200

    First commit

---
+++ google-group-manager.js
@@ -0,0 +1,99 @@
+var google = require('googleapis');
+
+function googleGroupManager(specs) {
+    "use strict";
+    var auth;
+    var service = google.admin('directory_v1');
+
+    function getGroups() {
+        var groupsSet = {
+            "kind": "admin#directory#groups",
+            groups: []
+        };
+
+        return new Promise(function (resolve, reject) {
+            function listGroups(pageToken) {
+                service.groups.list({
+                    auth: auth,
+                    fields: "nextPageToken, groups/name, groups/email",
+                    customer: 'my_customer',
+                    maxResults: 250,
+                    pageToken: pageToken
+                }, function (err, response) {
+                    if (err) {
+                        reject('The API returned an error: ' + err);
+                        return;
+                    }
+                    var groups = response.groups;
+
+                    if (groups.length === 0) {
+                        resolve(groupsSet);
+                        return;
+                    }
+                    groups.forEach(function (group) {
+                        groupsSet.groups.push([{
+                            name: group.name,
+                            emal: group.email
+                        }]);
+                    });
+                    if (!response.nextPageToken) {
+                        resolve(groupsSet);
+                        return;
+                    }
+                    getGroups(response.nextPageToken);
+                });
+            }
+            listGroups();
+        });
+    }
+
+    function getGroupMembers(specs) {
+        var groupEmail = specs.groupEmail;
+        var memberSet = {
+            "kind": "admin#directory#members",
+            members: []
+        };
+
+        return new Promise(function (resolve, reject) {
+            function listGroupMembers(pageToken) {
+                service.members.list({
+                    groupKey: groupEmail,
+                    auth: auth,
+                    fields: "nextPageToken, members",
+                    maxResults: 250,
+                    pageToken: pageToken
+                }, function (err, response) {
+                    if (err) {
+                        console.log('The API returned an error: ' + err);
+                        return;
+                    }
+                    var members = response.members;
+
+                    if (members.length === 0) {
+                        resolve(memberSet);
+                        return;
+                    }
+                    members.forEach(function (group) {
+                        memberSet.members.push([members]);
+                    });
+                    if (!response.nextPageToken) {
+                        resolve(memberSet);
+                        return;
+                    }
+                    getGroupMembers(response.nextPageToken);
+                });
+            }
+            listGroupMembers();
+        });
+    }
+
+    auth = specs.auth;
+    return {
+        getGroups: getGroups,
+        getGroupMembers: getGroupMembers
+    };
+}
+
+module.exports = {
+    googleGroupManager: googleGroupManager
+};
